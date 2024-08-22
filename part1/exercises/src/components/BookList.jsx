d719d52b7ab5429e45cd07a1c8b148f7c763d4ca
SynDecency <syndecency@gmail.com>
2024-08-22 17:26:36 -0500

    Part2

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,17 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "Book List";
+  let book1 =
+    "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTBoJZ8HunXcsp85V6j1l-dzA2c4ShK0v4IwM9tv3ubdL7stBhbYb9-qR4xpWXkOV0hsYjsOK_Z8C8yhhcfWgh_7A19REbX&usqp=CAc";
+  let book2 = "https://m.media-amazon.com/images/I/81tBoQP5V+L._SY522_.jpg";
+  let book3 =
+    "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRVrn_WB7Ck2CisS2K59dp5AKA-Ogaqk_a6HPxELf8cISs_6RZuX2AEc43qWXUqxrL-peX1D_cl9hs_QfoMvS7pSii-AVx18ofuHbhsLjqxMwcQsTgtpGGmHA&usqp=CAc";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="The Innocent Anthropologist by Nigel Barley" />
+      <img src={book2} alt="Wheel of Time by Robert Jordan" />
+      <img src={book3} alt="Lunch in Paris by Elizabeth Bard" />
+    </div>
+  );
 }
